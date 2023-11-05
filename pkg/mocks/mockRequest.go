e75f95e255533d5ec2e34c4e33bb684057fa336d
shubham <shubhamdixit863@gmail.com>
2023-11-06 02:38:00 +0530

    Added pkgs for e2e tests

---
+++ pkg/mocks/mockRequest.go
@@ -0,0 +1,28 @@
+package mocks
+
+import (
+	"time"
+
+	"github.com/numaproj/numaflow-go/pkg/sourcer"
+)
+
+type ReadRequest struct {
+	CountValue uint64
+	Timeout    time.Duration
+}
+
+func (r ReadRequest) TimeOut() time.Duration {
+	return r.Timeout
+}
+
+func (r ReadRequest) Count() uint64 {
+	return r.CountValue
+}
+
+type TestAckRequest struct {
+	OffsetsValue []sourcer.Offset
+}
+
+func (ar TestAckRequest) Offsets() []sourcer.Offset {
+	return ar.OffsetsValue
+}
